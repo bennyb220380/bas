88c5496ba99c5a2105d8501aecfb1ecba142b7fa
bennyb220380 <benny.b220380@gmail.com>
2017-02-01 16:27:44 +0000

    Add files via upload

---
+++ bunny/footer.php
@@ -0,0 +1,21 @@
+
+<?php
+	if (is_active_sidebar('footer_widget')){
+		echo '<div id="footer"><ul>';
+			dynamic_sidebar('footer_widget');
+		echo '</ul></div>';
+	}
+?>
+</div>
+<div class="clear"></div>
+<div id="kaninsmall" class="kaninsmall"></div>
+<?php
+/*Add easter eggs and christmas*/
+if( get_theme_mod( 'bunny_easter_eggs' ) <> '' || get_theme_mod( 'bunny_christmas' ) <> '') {
+	echo '<div class="egg2"></div>
+	<div class="egg1"></div>';		
+}
+
+wp_footer(); ?>
+</body>
+</html>
---
+++ bunny/sidebar.php
@@ -0,0 +1,5 @@
+<div id="sidebar">
+	<ul class="sidebar_inner">
+		<?php dynamic_sidebar('sidebar_widget');?>
+	</ul>
+</div>
